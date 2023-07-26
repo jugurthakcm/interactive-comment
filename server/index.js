c8c2b018e2db08483826882497db615d61f84bf3
jugurthakcm <jugurthakacimi1@gmail.com>
2023-07-26 17:25:56 +0000

    config server and db

--- server/index.js
+++ server/index.js
@@ -1,9 +1,17 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(express.json());
+
+console.log(process.env.MONGODB_URI);
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("Connected to Database"))
+  .catch((err) => console.error(err));
 
 const PORT = process.env.PORT || 3001;
 
